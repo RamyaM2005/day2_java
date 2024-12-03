91dd94ab7d10d919418a96638974bc8267431999
RamyaM2005 <ramyareddym023@gmail.com>
2024-12-03 14:19:18 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,16 @@
+public class Swap {
+    public static void main(String[] args) {
+        int boy=40,girl=30;
+        int temp;
+        System.out.println("before swap boy:"+boy);
+        System.out.println("before swap girl:"+girl);
+        temp=girl;
+        girl=boy;
+        boy=temp;
+        System.out.println("After swap boy:"+boy);
+        System.out.println("After swap girl:"+girl);
+
+
+    }
+    
+}
