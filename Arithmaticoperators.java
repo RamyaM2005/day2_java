5ef745c86e166bae4b62b1196d8730a67c1e0040
RamyaM2005 <ramyareddym023@gmail.com>
2024-12-03 10:15:10 +0530

    Add files via upload

---
+++ Arithmaticoperators.java
@@ -0,0 +1,12 @@
+public class Arithmaticoperators {
+    public static void main(String[] args) {
+        int a=10,b=20;
+        int c=a+b,d=a-b,e=a*b,f=a/b,g=a%b;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+
+}
+}
